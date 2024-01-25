d01e853e8ffcae407d4d3cb1cf20af89cacc10b6
Jonas Hietala <mail@jonashietala.se>
2024-01-25 19:59:40 +0100

    WIP

--- src/djot/mod.rs
+++ src/djot/mod.rs
@@ -3,14 +3,13 @@ mod transform_headers;
 use eyre::Result;
 use jotdown::{html, Parser, Render};
 
+use self::auto_figures::AutoFigures;
 use self::transform_headers::TransformHeaders;
 
 pub fn djot_to_html(djot: &str) -> Result<String> {
-    let transformed = Parser::new(djot).map(|x| {
-        // dbg!(&x);
-        x
-    });
+    let transformed = Parser::new(djot);
     let transformed = TransformHeaders::new(transformed);
+    let transformed = AutoFigures::new(transformed);
 
     let mut body = String::new();
     html::Renderer::default().push(transformed, &mut body)?;
