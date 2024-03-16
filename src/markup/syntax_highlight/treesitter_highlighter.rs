fc13b93dd0d15a0f0a4c6d6e543f2d19c185ffa5
Jonas Hietala <mail@jonashietala.se>
2024-03-16 20:35:22 +0100

    WIP treesitter, use nvim-treesitter queries?

--- src/markup/syntax_highlight/treesitter_highlighter.rs
+++ src/markup/syntax_highlight/treesitter_highlighter.rs
@@ -1,5 +1,6 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, fs};
 
+use camino::Utf8PathBuf;
 use eyre::Result;
 use lazy_static::lazy_static;
 use tree_sitter_highlight::{HighlightConfiguration, HighlightEvent, Highlighter};
@@ -17,7 +18,7 @@ impl<'a> TreesitterHighlighter<'a> {
         let mut highlighter = Highlighter::new();
 
         let highlights = highlighter
-            .highlight(self.config, code.as_bytes(), None, |_| None)
+            .highlight(self.config, code.as_bytes(), None, |x| CONFIGS.get(x))
             .unwrap();
 
         // This isn't very nice... How to generate strings dynamically from inside a Fn closure
@@ -57,6 +58,7 @@ impl<'a> TreesitterHighlighter<'a> {
 // TODO more things...
 static HIGHLIGHT_NAMES: &[&str] = &[
     "attribute",
+    "comment",
     "constant",
     "function.builtin",
     "function",
@@ -66,7 +68,6 @@ static HIGHLIGHT_NAMES: &[&str] = &[
     "punctuation",
     "punctuation.bracket",
     "punctuation.delimiter",
-    "punctuation.delimiter.regex",
     "string",
     "string.special",
     "tag",
@@ -82,8 +83,36 @@ lazy_static! {
     //     .iter()
     //     .map(|name| format!(r#"class="{}""#, name.replace(".", " ")))
     //     .collect();
+    static ref HOME: Utf8PathBuf = Utf8PathBuf::from_path_buf(dirs::home_dir().unwrap()).unwrap();
     static ref CONFIGS: HashMap<String, HighlightConfiguration> = init_configurations();
 }
+
+fn read_nvim_treesitter_file(lang: &str, file: &str) -> String {
+    let path = HOME
+        .join(".local/share/nvim/lazy/nvim-treesitter/queries/")
+        .join(lang)
+        .join(file);
+    let content =
+        fs::read_to_string(&path).expect(&format!("Failed to find nvim-treesitter file: {path}"));
+    content
+}
+
+fn read_nvim_treesitter_highlights(lang: &str) -> String {
+    read_nvim_treesitter_file(lang, "highlights.scm")
+}
+
+fn read_nvim_treesitter_injections(lang: &str) -> String {
+    read_nvim_treesitter_file(lang, "injections.scm")
+}
+
+fn read_nvim_treesitter_locals(lang: &str) -> String {
+    read_nvim_treesitter_file(lang, "locals.scm")
+}
+
+// struct Config {
+//     lang: &'static str,
+//     langauge: Language,
+// }
 
 fn init_configurations() -> HashMap<String, HighlightConfiguration> {
     [
@@ -101,9 +130,9 @@ fn init_configurations() -> HashMap<String, HighlightConfiguration> {
             "c",
             HighlightConfiguration::new(
                 tree_sitter_c::language(),
-                tree_sitter_c::HIGHLIGHT_QUERY,
-                "",
-                "",
+                &dbg!(read_nvim_treesitter_highlights("c")),
+                &read_nvim_treesitter_injections("c"),
+                &read_nvim_treesitter_locals("c"),
             )
             .unwrap(),
         ),
@@ -122,7 +151,11 @@ mod tests {
     #[test]
     fn test_treesitter_highlight() {
         let highlighter = TreesitterHighlighter::find("rust").unwrap();
-        assert_eq!(highlighter.highlight("rust", "let x = 2;").unwrap(), "<span class=\"keyword rust\">let</span> x = <span class=\"constant rust\">2</span><span class=\"punctuation delimiter rust\">;</span>\n");
+        assert_eq!(
+            highlighter.highlight("rust", "let x = 2;").unwrap(),
+            "<span class=\"keyword rust\">let</span> x = <span class=\"constant rust\">2</span><span class=\"punctuation delimiter rust\">;</span>\n"
+        );
+        // assert!(false);
 
         //         assert_eq!(
         //             highlighter
